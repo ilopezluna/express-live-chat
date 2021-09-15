9dfad176e938bd5450ea2fe3855de411e2b5b649
ilopezluna <ignasi.lopez.luna@gmail.com>
2021-09-15 17:18:57 +0200

    Add chat

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.get('/', (req, res) => {    
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
@@ -14,8 +14,12 @@ io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
-      });
-  });
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
